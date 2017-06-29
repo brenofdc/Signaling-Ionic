93621d26ba8f7ef45f739cefbc4eb6727a0658a3
Johnathan <johnathan_gomes@outlook.com>
2017-06-29 13:04:07 -0300

    Criando resource para os casos

---
+++ www/js/resource/report.resource.js
@@ -0,0 +1,98 @@
+(function () {
+    angular
+        .module('signaling')
+        .factory('ReportResource', Service);
+    Service.$inject = ['$q', 'ResourceFactory'];
+    function Service($q, ResourceFactory) {
+        var Resource = ResourceFactory.resource("/api/report/:id/:action");
+        return {
+            createReport: function (reportData) {
+                var newReport = new Resource(reportData);
+                var promise = $q.defer();
+                newReport.$save().then(
+                    function(){
+                      promise.resolve();
+                    },
+                    function(response){
+                        promise.reject(response.reason);
+                    }
+                );
+
+                return promise.promise;
+            },
+            closeReport: function (id) {
+                var report = new Resource();
+                var promise = $q.defer();
+                report.$update({id: id, action: 'close'}).then(
+                    function(){
+                      promise.resolve();
+                    },
+                    function(response){
+                      promise.reject(response.reason);
+                    }
+                );
+
+                return promise.promise;
+            },
+            getReport: function(id){
+                var report = new Resource();
+                var promise = $q.defer();
+                report.$get({id: id}).then(
+                    function(response){
+                      promise.resolve(response.data);
+                    },
+                    function(response){
+                        promise.reject(response.reason);
+                    }
+                );
+
+                return promise.promise;
+            },
+            getAllReports: function(){
+                var reports = new Resource();
+
+                var promise = $q.defer();
+                reports.$get().then(
+                    function(response){
+                      promise.resolve(response.data);
+                    },
+                    function(response){
+                        promise.reject(response.reason);
+                    }
+                );
+
+                return promise.promise;
+            },
+            editReport: function(id, newReportData){
+                var report = new Resource(newReportData);
+
+                var promise = $q.defer();
+                report.$update({id: id}).then(
+                    function(response){
+                      promise.resolve(response.data);
+                    },
+                    function(response){
+                      promise.reject(response.reason);
+                    }
+                );
+
+                return promise.promise;
+            },
+            deleteReport: function(id){
+                var report = new Resource();
+
+                var promise = $q.defer();
+                report.$delete({id: id}).then(
+                    function(){
+                      promise.resolve();
+                    },
+                    function(response){
+                        promise.reject(response.reason);
+                    }
+                );
+
+                return promise.promise;
+            }
+        }
+    }
+})();
