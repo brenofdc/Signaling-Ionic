5f2e92ad8e55bcca80249b42b88a3a7da2ec4b30
Breno Fernandes <brenofdc@gmail.com>
2017-06-29 14:46:53 -0300

    Criando modal e controller pra registro de usuário

--- www/js/main/index.controller.js
+++ www/js/main/index.controller.js
@@ -3,8 +3,8 @@
         .module('signaling')
         .controller('IndexCtrl', Controller);
 
-    Controller.$inject = ['$ionicSideMenuDelegate', '$state', 'Popup', 'Session', 'AuthResource'];
-    function Controller($ionicSideMenuDelegate, $state, Popup, Session, AuthResource) {
+    Controller.$inject = ['$scope', '$ionicSideMenuDelegate', '$ionicModal', '$state', 'Popup', 'Session', 'AuthResource'];
+    function Controller($scope, $ionicSideMenuDelegate, $ionicModal, $state, Popup, Session, AuthResource) {
       var vm = this;
 
       vm.loginData = {
@@ -13,8 +13,14 @@
       };
 
       vm.login = login;
+      vm.startRegister = startRegister;
+      vm.cancelRegister = cancelRegister;
+      vm.finishRegister = finishRegister;
 
       $ionicSideMenuDelegate.canDragContent(false);
+      $ionicModal.fromTemplateUrl('register-modal.html', {scope: $scope}).then(function(modal) {
+        vm.modal = modal;
+      });
 
       function login(){
         if (vm.loginData.email === "" || vm.loginData.password === ""){
@@ -35,6 +41,58 @@
                 }
               });
         }
+      }
+
+      function startRegister(){
+        vm.makingRegister = false;
+
+        vm.newUser = {
+          cpf: "",
+          name: "",
+          email: "",
+          password: "",
+          repeatPassword: ""
+        };
+
+        vm.modal.show();
+      }
+
+      function cancelRegister(){
+        vm.modal.hide();
+      }
+
+      function finishRegister(){
+        for (var attr in vm.newUser){
+          if (!vm.newUser[attr]){
+            Popup.error('Preencha todos os campos corretamente.');
+            return false;
+          }
+        }
+        if (vm.newUser.password.length < 6){
+          Popup.error('A senha deve conter no mínimo 6 caracteres.');
+          return false;
+        }
+        if (vm.newUser.password !== vm.newUser.repeatPassword){
+          Popup.error('As senhas informadas não coincidem.');
+          return false;
+        }
+        vm.makingRegister = true;
+        AuthResource.createUser(vm.newUser).then(function(){
+            Popup.success('Usuário criado com sucesso!');
+            vm.makingRegister = false;
+            vm.modal.hide();
+          },
+          function(reason){
+            vm.makingRegister = false;
+            if (reason === "duplicated"){
+              Popup.error('O email informado já possui um cadastro.');
+              return false;
+            }
+            else{
+              Popup.error('Não foi possível comunicar com o servidor. Verifique sua conexão com a internet.');
+              return false;
+            }
+          });
       }
     }
 })();
