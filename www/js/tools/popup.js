3264f0270dd4d4a59eae6b3f6b1e20fc8555c890
Johnathan <johnathan_gomes@outlook.com>
2017-06-29 13:17:03 -0300

    Criando serviço pra popups

---
+++ www/js/tools/popup.js
@@ -0,0 +1,23 @@
+(function () {
+    angular
+        .module('signaling')
+        .factory('Popup', Service);
+
+    Service.$inject = ['$ionicPopup'];
+    function Service($ionicPopup) {
+        return {
+            success: function(msg){
+                $ionicPopup.alert({
+                    title: msg,
+                    okType: 'button-balanced'
+                });
+            },
+            error: function(msg){
+                $ionicPopup.alert({
+                    title: msg,
+                    okType: "button-assertive"
+                });
+            }
+        }
+    }
+})();
