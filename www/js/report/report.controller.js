cf41836fad4a2a6bb059420774ffeb47435a17d7
Johnathan <johnathan_gomes@outlook.com>
2017-06-30 23:57:07 -0300

    Adicionando checagem a mapa

--- www/js/report/report.controller.js
+++ www/js/report/report.controller.js
@@ -3,12 +3,15 @@
     .module('signaling')
     .controller('ReportCtrl', Controller);
 
-  Controller.$inject = ['$state', 'ReportResource'];
-  function Controller($state, ReportResource) {
+  Controller.$inject = ['$state', 'ReportResource', 'Session'];
+  function Controller($state, ReportResource, Session) {
 
     var vm = this;
 
     vm.id = $state.params.reportId;
+
+    vm.canManage = canManage;
+    vm.hasLatLong = hasLatLong;
 
     function loadReport(){
       ReportResource.getReport(vm.id)
@@ -18,6 +21,24 @@
         function(){
           $state.go('reportList');
         });
+    }
+
+    function canManage(){
+      if (!vm.report){
+        return false;
+      }
+
+      return vm.report.status !== 'closed' &&
+          Session.hasUserAuthenticated() &&
+          Session.getCurrentUser().id === vm.report.user_id;
+    }
+
+    function hasLatLong(){
+      if (!vm.report){
+        return false;
+      }
+
+      return !!vm.report.lat && !!vm.report.long;
     }
 
     loadReport();
