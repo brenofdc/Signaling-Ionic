fceaa2631e097159d3ebe15abef447527bf01028
Breno Fernandes <brenofdc@gmail.com>
2017-07-01 11:27:38 -0300

    Criando checagem de autenticação para telas

--- www/js/app.js
+++ www/js/app.js
@@ -5,9 +5,19 @@
   angular.module('signaling')
     .run(OnRun);
 
-  OnRun.$inject = ['$ionicPlatform'];
+  OnRun.$inject = ['$ionicPlatform', '$rootScope', '$state'];
 
-  function OnRun($ionicPlatform) {
+  function OnRun($ionicPlatform, $rootScope, $state) {
+
+    $rootScope.$on('$stateChangeError', function (event, toState, toParams, fromState, fromParams, error) {
+      if (error === 'no_auth'){
+        $state.go('index');
+      }
+      else if (error === 'already_auth'){
+        $state.go('reportList');
+      }
+    });
+
     $ionicPlatform.ready(function() {
       if(window.cordova && window.cordova.plugins.Keyboard) {
 
--- www/js/routes.js
+++ www/js/routes.js
@@ -1,36 +1,48 @@
 (function() {
-    angular
-        .module('signaling')
-        .config(Routes);
+  angular
+    .module('signaling')
+    .config(Routes);
 
-    Routes.$inject = ['$stateProvider', '$urlRouterProvider'];
-    function Routes($stateProvider, $urlRouterProvider) {
+  Routes.$inject = ['$stateProvider', '$urlRouterProvider'];
+  function Routes($stateProvider, $urlRouterProvider) {
 
-
-        $stateProvider
-            .state('index', {
-                url: '/index',
-                cache: false,
-                templateUrl: 'templates/main/index.html',
-                controller: 'IndexCtrl',
-                controllerAs: 'vm'
-            })
-            .state('reportList', {
-                url: '/reportList',
-                cache: false,
-                templateUrl: 'templates/report/list.html',
-                controller: 'ReportListCtrl',
-                controllerAs: 'vm'
-            })
-            .state('report', {
-                url: '/report/:reportId',
-                cache: false,
-                templateUrl: 'templates/report/report.html',
-                controller: 'ReportCtrl',
-                controllerAs: 'vm'
-            });
-
-        $urlRouterProvider.otherwise('/index');
-
+    Authenticated.$inject = ['$q', 'Session'];
+    function Authenticated($q, Session){
+      return Session.hasUserAuthenticated() ? $q.resolve() : $q.reject('no_auth');
     }
+
+    NotAuthenticated.$inject = ['$q', 'Session'];
+    function NotAuthenticated($q, Session){
+      return Session.hasUserAuthenticated() ? $q.reject('already_auth') : $q.resolve();
+    }
+
+    $stateProvider
+      .state('index', {
+        url: '/index',
+        cache: false,
+        templateUrl: 'templates/main/index.html',
+        controller: 'IndexCtrl',
+        controllerAs: 'vm',
+        resolve: {
+          noAuth: NotAuthenticated
+        }
+      })
+      .state('reportList', {
+        url: '/reportList',
+        cache: false,
+        templateUrl: 'templates/report/list.html',
+        controller: 'ReportListCtrl',
+        controllerAs: 'vm'
+      })
+      .state('report', {
+        url: '/report/:reportId',
+        cache: false,
+        templateUrl: 'templates/report/report.html',
+        controller: 'ReportCtrl',
+        controllerAs: 'vm'
+      });
+
+    $urlRouterProvider.otherwise('/index');
+
+  }
 })();
