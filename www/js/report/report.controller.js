a71fe0662a3f076e3931288d2e555e5a86db7d65
Johnathan <johnathan_gomes@outlook.com>
2017-07-01 00:02:04 -0300

    Criando ação de deletar e fechar caso

--- www/js/report/report.controller.js
+++ www/js/report/report.controller.js
@@ -3,8 +3,8 @@
     .module('signaling')
     .controller('ReportCtrl', Controller);
 
-  Controller.$inject = ['$state', 'ReportResource', 'Session'];
-  function Controller($state, ReportResource, Session) {
+  Controller.$inject = ['$state', '$ionicPopup', 'ReportResource', 'Session'];
+  function Controller($state, $ionicPopup, ReportResource, Session) {
 
     var vm = this;
 
@@ -12,6 +12,8 @@
 
     vm.canManage = canManage;
     vm.hasLatLong = hasLatLong;
+    vm.deleteReport = deleteReport;
+    vm.closeReport = closeReport;
 
     function loadReport(){
       ReportResource.getReport(vm.id)
@@ -39,6 +41,44 @@
       }
 
       return !!vm.report.lat && !!vm.report.long;
+    }
+
+    function deleteReport(){
+      $ionicPopup.show({
+        template: '',
+        title: 'Deletar caso',
+        subTitle: 'Deseja mesmo deletar este caso? Esta ação é irreversível.',
+        buttons: [
+          { text: 'Não' },
+          {
+            text: '<b>Sim</b>',
+            type: 'button-assertive',
+            onTap: function(e) {
+              ReportResource.deleteReport(vm.id);
+              $state.go("reportList");
+            }
+          }
+        ]
+      });
+    }
+
+    function closeReport(){
+      $ionicPopup.show({
+        template: '',
+        title: 'Finalizar caso',
+        subTitle: 'Deseja mesmo marcar este caso como finalizado? Esta ação é irreversível.',
+        buttons: [
+          { text: 'Não' },
+          {
+            text: '<b>Sim</b>',
+            type: 'button-balanced',
+            onTap: function(e) {
+              ReportResource.closeReport(vm.id);
+              $state.go("reportList");
+            }
+          }
+        ]
+      });
     }
 
     loadReport();
