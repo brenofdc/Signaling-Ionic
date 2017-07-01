c1fddef7299b1912052d5b22cd782a780951fa8e
Johnathan <johnathan_gomes@outlook.com>
2017-07-01 13:32:35 -0300

    Implementando busca na lista de casos

--- www/js/report/report_list.controller.js
+++ www/js/report/report_list.controller.js
@@ -9,9 +9,13 @@
     var vm = this;
 
     vm.reports = [];
+    vm.search = "";
     vm.fetching = false;
 
+
     vm.goToReport = goToReport;
+    vm.matchesSearch = matchesSearch;
+    vm.isShowingReports = isShowingReports;
 
     function loadReports(){
       vm.fetching = true;
@@ -25,6 +29,39 @@
 
     function goToReport(id){
       $state.go('report', {reportId: id});
+    }
+
+    function matchesSearch(report){
+      if (!!vm.search){
+        var search = vm.search.toLowerCase();
+        if (report.id.toString().toLowerCase().indexOf(search) !== -1){
+          return true;
+        }
+        if (report.description.toLowerCase().indexOf(search) !== -1){
+          return true;
+        }
+        if (report.proposal.toLowerCase().indexOf(search) !== -1){
+          return true;
+        }
+
+        return report.address.toLowerCase().indexOf(search) !== -1;
+      }
+
+      return true;
+    }
+
+    function isShowingReports(){
+      if (!vm.reports || !vm.reports.length){
+        return false;
+      }
+
+      for (var i = 0; i < vm.reports.length; i++){
+        if (matchesSearch(vm.reports[i])){
+          return true;
+        }
+      }
+
+      return false;
     }
 
     loadReports();
